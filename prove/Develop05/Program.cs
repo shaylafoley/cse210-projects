d862b1c093096271fd7aeb2453fd467ebc64b1a8
shaylafoley <wholemotherhood@gmail.com>
2024-03-31 00:54:39 -0400

    Extra points

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,7 +8,7 @@ class Program
     //Extra things for the Surpassing Expectations
     //Put an extra message if the person accomplishes the bonus
     //Message for bonus includes a bunch of stars
-    //Extra option in menu to display the score again.
+    //Extra option in menu to display the score as often as the user would like.
     static void Main(string[] args)
     {
         GoalManager manager = new GoalManager();
